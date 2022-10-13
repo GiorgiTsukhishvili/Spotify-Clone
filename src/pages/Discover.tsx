7ed034f042e202d88bc6728607547ed9927937b2
Giorgi Tsukhishvili <gcuxishvili@gmail.com>
2022-10-13 15:49:23 +0400

    Adding redux files and other stuff

--- src/pages/Discover.tsx
+++ src/pages/Discover.tsx
@@ -1,3 +1,15 @@
-const Discover = () => <div>Discover</div>;
+import React from "react";
+import { Error, Loader, SongCard } from "../components";
+import { genres } from "./../assets/data/constants";
+
+const Discover = () => {
+  return (
+    <div className="flex flex-col">
+      <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+        <select name="" id=""></select>
+      </div>
+    </div>
+  );
+};
 
 export default Discover;
