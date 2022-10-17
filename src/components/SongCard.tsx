5a0f7188d4697899a64d95e45c6fd7d1008a2770
Giorgi Tsukhishvili <gcuxishvili@gmail.com>
2022-10-17 22:27:16 +0400

    Commit before starting to work on each song

--- src/components/SongCard.tsx
+++ src/components/SongCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
+import { SongInterface } from "../pages/interfaces/songInterface";
 
-const SongCard = () => {
+const SongCard = ({ song, i }: { song: SongInterface; i: number }) => {
   return <div>SongCard</div>;
 };
 
--- src/pages/Discover.tsx
+++ src/pages/Discover.tsx
@@ -34,7 +34,7 @@ const Discover = () => {
         </select>
       </div>
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
-        {data?.map((song: SongInterface, i) => (
+        {data?.map((song: SongInterface, i: number) => (
           <SongCard key={song.key} song={song} i={i} />
         ))}
       </div>
