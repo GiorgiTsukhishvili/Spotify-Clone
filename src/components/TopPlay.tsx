e01822f0276ee46439905d5a53f56ee7abe0c7e1
Giorgi Tsukhishvili <gcuxishvili@gmail.com>
2022-10-19 21:58:24 +0400

    Commit at the end of the day

--- src/components/TopPlay.tsx
+++ src/components/TopPlay.tsx
@@ -63,6 +63,41 @@ const TopPlay = () => {
           ))}
         </div>
       </div>
+
+      <div className=" flex flex-col mt-8">
+        <div className="flex flex-row justify-between items-center">
+          <h2 className="text-white font-bold text-xl">Top Artists</h2>
+          <Link to="/top-artists">
+            <p className="text-gray-300 text-base cursor-pointer">See more</p>
+          </Link>
+        </div>
+
+        <Swiper
+          slidesPerView="auto"
+          spaceBetween={15}
+          freeMode
+          centeredSlides
+          centeredSlidesBounds
+          modules={[FreeMode]}
+          className="mt-4"
+        >
+          {topPlays.map((song: SongInterface, i: number) => (
+            <SwiperSlide
+              key={song.key}
+              style={{ width: "10%", height: "auto" }}
+              className="shadow-lg rounded-full animate-slideright"
+            >
+              <Link to={`/artists/${song.artists[0].adamid}`}>
+                <img
+                  src={song.images.background}
+                  alt="name"
+                  className="rounded-full  object-cover"
+                />
+              </Link>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
   );
 };
