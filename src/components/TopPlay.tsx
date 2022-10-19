28b82d8b7b3e71c34f79df759f76d73eb9e64126
Giorgi Tsukhishvili <gcuxishvili@gmail.com>
2022-10-19 21:41:43 +0400

    Creating TopPlays

--- src/components/TopPlay.tsx
+++ src/components/TopPlay.tsx
@@ -1,5 +1,70 @@
-const TopPlay = () => (
-  <div>TopPlay</div>
+import React, { useEffect, useRef } from "react";
+
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode } from "swiper";
+
+import PlayPause from "./PlayPause";
+import {
+  playerReducer,
+  playPause,
+  setActiveSong,
+} from "../redux/features/playerSlice";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+
+import "swiper/css";
+import "swiper/css/free-mode";
+import { SongInterface } from "../pages/interfaces/songInterface";
+
+const TopChartCart = ({ song, i }: { song: SongInterface; i: number }) => (
+  <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2">
+    {song.title}
+  </div>
 );
+
+const TopPlay = () => {
+  const dispatch = useDispatch();
+
+  const { activeSong, isPlaying } = useSelector(playerReducer);
+  const { data } = useGetTopChartsQuery(null);
+  const divRef: any = useRef(null);
+
+  const topPlays = data?.slice(0, 5);
+
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
+
+  const handlePlayClick = () => {
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
+  };
+
+  useEffect(() => {
+    divRef.current!.scrollIntoView({ behavior: "smooth" });
+  });
+
+  return (
+    <div
+      ref={divRef}
+      className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xs:max-w-[500px] max-w-full flex flex-col"
+    >
+      <div className="w-full flex flex-col">
+        <div className="flex flex-row justify-between items-center">
+          <h2 className="text-white font-bold text-xl">Top Charts</h2>
+          <Link to="/top-charts">
+            <p className="text-gray-300 text-base cursor-pointer">See more</p>
+          </Link>
+        </div>
+        <div className="mt-4 flex flex-col gap-1">
+          {topPlays.map((song: SongInterface, i: number) => (
+            <TopChartCart song={song} i={i} key={song.key} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default TopPlay;
