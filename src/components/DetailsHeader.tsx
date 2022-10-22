0bf19665103316a5e1c439f16a29c94a2717c9fa
Giorgi Tsukhishvili <gcuxishvili@gmail.com>
2022-10-22 21:33:29 +0400

    commit before creating related songs

--- src/components/DetailsHeader.tsx
+++ src/components/DetailsHeader.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { TrackInterface } from "../pages/interfaces/trackInterface";
 
 const DetailsHeader = ({
@@ -12,11 +13,11 @@ const DetailsHeader = ({
 }) => {
   return (
     <div className="relative w-full flex flex-col">
-      <div className="w-full bg-gradient-to-l from-transpatent to-black sm:h-48 h-28"></div>
+      <div className="w-full bg-gradient-to-l from-transparent to-black sm:h-48 h-28"></div>
       <div className="absolute inset-0 flex items-center">
         <img
           src={
-            artistId.length > 0
+            artistId.length > 0 && songData && artistData
               ? artistData?.artists[artistId].attributes?.artwork?.url
                   .replace("{w}", "500")
                   .replace("{h}", "500")
@@ -25,7 +26,28 @@ const DetailsHeader = ({
           alt="Art"
           className="sm:w-48 w-28 sm:h-48 h-28 roudned-full object-cover border-2 shadow-xl shadow-black"
         />
+        <div className="ml-5">
+          <p className="font-bold sm:text-3xl text-xl text-white">
+            {artistId.length > 0 && songData && artistData
+              ? artistData.artists[artistId].attributes.name
+              : songData?.title}
+          </p>
+          {artistId === "" && (
+            <Link to={`/artists/${songData?.artists[0].adamid}`}>
+              <p className="text-base text-gray-400 mt-2">
+                {songData?.subtitle}
+              </p>
+            </Link>
+          )}
+          <p className="text-base text-gray-400 mt-2">
+            {artistId.length > 0
+              ? artistData?.genreNames[0]
+              : songData?.genres?.primary}
+          </p>
+        </div>
       </div>
+
+      <div className="w-full sm:h-44 h-24"></div>
     </div>
   );
 };
--- src/pages/SongDetails.tsx
+++ src/pages/SongDetails.tsx
@@ -38,6 +38,8 @@ const SongDetails = () => {
           )}
         </div>
       </div>
+
+      <RelatedSongs />
     </div>
   );
 };
